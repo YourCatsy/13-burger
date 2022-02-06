ed6fc2744731fa21914357fc0c0870439bb821d7
Catherine_AMF <catherineamf@gmail.com>
2022-02-06 20:42:59 +0200

    create Hamburger using constructor

--- script.js
+++ script.js
@@ -1,97 +1,86 @@
 'use strict';
 
-class Hamburger {
-    constructor(size) {
-        this.#baseSize = size;
-    }
-
-    #baseSize = {
-        price: 0,
-
-        callories: 0,
+function FastFood() {
+    this.getCallories = function () {
+        return this.getSum(FastFood.CALLORIES_FIELD);
     };
-
-    #toppings = [];
-
-    static SIZE_SMALL = {
-        price: 50,
-
-        callories: 20,
+    this.getPrice = function () {
+        return this.getSum(FastFood.PRICE_FIELD);
     };
-
-    static SIZE_MEDIUM = {
-        price: 75,
-
-        callories: 30,
-    };
-
-    static SIZE_BIG = {
-        price: 100,
-
-        callories: 40,
-    };
-
-    static TOPPING_CHEESE = {
-        price: 10,
-
-        callories: 20,
-    };
-
-    static TOPPING_MAYO = {
-        price: 20,
-
-        callories: 5,
-    };
-    static TOPPING_SALADE = {
-        price: 20,
-
-        callories: 5,
-    };
-
-    static TOPPING_POTATO = {
-        price: 15,
-
-        callories: 10,
-    };
-
-    static TOPPING_FLAVORING = {
-        price: 15,
-
-        callories: 0,
-    };
-
-    getPrice() {
-        return this.getSum('price');
-    }
-
-    getCallories() {
-        return this.getSum('callories');
-    }
-
-    getSum(propertyName) {
-        return this.#baseSize[propertyName] + this.#toppings.reduce((previous, current) => {
+    this.getSum = function (propertyName) {
+        return this.baseSize[propertyName] + this.toppings.reduce((previous, current) => {
             return previous + current[propertyName];
         }, 0);
-    }
-
-    addTopping(topping) {
-        if (topping && this.isNotSize(topping) && this.isToppingValid(topping)) {
-            this.#toppings.push(topping);
-        }
-    }
-
-    isNotSize(object) {
-        return object !== Hamburger.SIZE_BIG && object !== Hamburger.SIZE_MEDIUM && object !== Hamburger.SIZE_SMALL;
-    }
-
-    isToppingValid(topping) {
-        return this.validPositiveNumberProperty(topping, 'price') && this.validPositiveNumberProperty(topping, 'callories');
-    }
-
-    validPositiveNumberProperty(topping, propertyName) {
+    };
+    this.validPositiveNumberProperty = function (topping, propertyName) {
         return topping.hasOwnProperty(propertyName) && !isNaN(topping[propertyName]) && topping[propertyName] >= 0;
-    }
+    };
+    this.isToppingValid = function (topping) {
+        return this.validPositiveNumberProperty(topping, FastFood.PRICE_FIELD) && this.validPositiveNumberProperty(topping, FastFood.CALLORIES_FIELD);
+    };
+    this.isNotSize = function (object) {
+        return object !== Hamburger.SIZE_BIG && object !== Hamburger.SIZE_MEDIUM && object !== Hamburger.SIZE_SMALL;
+    };
+    this.addTopping = function (topping) {
+        if (topping && this.isNotSize(topping) && this.isToppingValid(topping)) {
+            this.toppings.push(topping);
+        }
+    };
 }
+
+FastFood.PRICE_FIELD = 'price';
+FastFood.CALLORIES_FIELD = 'callories';
+
+function Hamburger(size) {
+    this.baseSize = {
+        price: 0,
+        callories: 0,
+    };
+    this.baseSize = size;
+    this.toppings = [];
+}
+
+Hamburger.SIZE_SMALL = {
+    price: 50,
+    callories: 20,
+};
+
+Hamburger.SIZE_MEDIUM = {
+    price: 75,
+    callories: 30,
+};
+
+Hamburger.SIZE_BIG = {
+    price: 100,
+    callories: 40,
+};
+
+Hamburger.TOPPING_CHEESE = {
+    price: 10,
+    callories: 20,
+};
+
+Hamburger.TOPPING_MAYO = {
+    price: 20,
+    callories: 5,
+};
+
+Hamburger.TOPPING_SALADE = {
+    price: 20,
+    callories: 5,
+};
+
+Hamburger.TOPPING_POTATO = {
+    price: 15,
+    callories: 10,
+};
+
+Hamburger.TOPPING_FLAVORING = {
+    price: 15,
+    callories: 0,
+};
+
+Hamburger.prototype = new FastFood();
 
 const hamburger = new Hamburger(Hamburger.SIZE_BIG);
 
